a5b13815779c03a5a3222fe7599e3f2a43e52fdc
Aravind Vaddi <aravindvaddi.github@gmail.com>
2020-10-30 05:23:34 -0400

    Add code to encrypt models, load key from environment at runtime and gitignore to keep the key it out of the git tree

--- GiftcardSite/settings.py
+++ GiftcardSite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import base64
+import environ
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,7 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kmgysa#fz+9(z1*=c0ydrjizk*7sthm2ga1z4=^61$cxcq8b$l'
+env = environ.Env()
+environ.Env.read_env()
+SECRET_KEY = env("SECRET_KEY")
+# SECRET_KEY = 'kmgysa#fz+9(z1*=c0ydrjizk*7sthm2ga1z4=^61$cxcq8b$l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- LegacySite/models.py
+++ LegacySite/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.backends import BaseBackend
 from . import extras
 
+from django_cryptography.fields import encrypt
+
 # Create your models here.
 class User(AbstractBaseUser):
     username = models.CharField(max_length=30, unique=True)
@@ -30,16 +32,16 @@ class OurBackend(BaseBackend):
 
 class Product(models.Model):
     product_id = models.AutoField(primary_key=True)
-    product_name = models.CharField(max_length=50, unique=True)
-    product_image_path = models.CharField(max_length=100, unique=True)
-    recommended_price = models.IntegerField()
-    description = models.CharField(max_length=250)
+    product_name = encrypt(models.CharField(max_length=50, unique=True))
+    product_image_path = encrypt(models.CharField(max_length=100, unique=True))
+    recommended_price = encrypt(models.IntegerField())
+    description = encrypt(models.CharField(max_length=250))
 
 class Card(models.Model):
     id = models.AutoField(primary_key=True)
     data = models.BinaryField(unique=True)
     product = models.ForeignKey('LegacySite.Product', on_delete=models.CASCADE, default=None)
-    amount = models.IntegerField()
-    fp = models.CharField(max_length=100, unique=True)
+    amount = encrypt(models.IntegerField())
+    fp = encrypt(models.CharField(max_length=100, unique=True))
     user = models.ForeignKey('LegacySite.User', on_delete=models.CASCADE)
     used = models.BooleanField(default=False)
